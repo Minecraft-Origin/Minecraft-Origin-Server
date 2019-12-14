79ea8dc2cf38dd217edad50cf66341a3b56af686
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-14 22:12:07 +0800

    Update git assume-unchanged.bat

--- git assume-unchanged.bat
+++ git assume-unchanged.bat
@@ -1,3 +1,4 @@
 git update-index --assume-unchanged "Minecraft Origin Server/server.properties"
 git update-index --assume-unchanged "Minecraft Origin Server/config/nucleus/commands.conf"
 git update-index --assume-unchanged "Minecraft Origin Server/config/nucleus/main.conf"
+git update-index --assume-unchanged "Minecraft Origin Server/config/flexiblelogin/config.conf"
