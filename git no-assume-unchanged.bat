75f3942ee01414fe0498953513afacfd420071d2
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-14 18:24:56 +0800

    Create git no-assume-unchanged.bat

---
+++ git no-assume-unchanged.bat
@@ -0,0 +1,3 @@
+git update-index --no-assume-unchanged "Minecraft Origin Server/server.properties"
+git update-index --no-assume-unchanged "Minecraft Origin Server/config/nucleus/commands.conf"
+git update-index --no-assume-unchanged "Minecraft Origin Server/config/nucleus/main.conf"
