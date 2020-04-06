302f9e7411ea1622aa0fc039dc001d5596ba6531
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-04-06 23:49:07 +0800

    Update git assume-unchanged.bat

--- git assume-unchanged.bat
+++ git assume-unchanged.bat
@@ -5,4 +5,5 @@ git update-index --assume-unchanged "Minecraft Origin Server/config/flexiblelogi
 git update-index --assume-unchanged "Minecraft Origin Server/config/flexiblelogin/locale.conf"
 git update-index --assume-unchanged "Minecraft Origin Server/config/reccomplex.cfg"
 git update-index --assume-unchanged "Minecraft Origin Server/config/battletowers.cfg"
+git update-index --assume-unchanged "Minecraft Origin Server/config/cyclicmagic.cfg"
 git update-index --assume-unchanged "Minecraft Origin Server/config/endercore/endercore.cfg"
